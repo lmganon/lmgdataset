ef277c19c057df36ab0268972ff1e4f3895c74bb
lmganon <135924006+lmganon@users.noreply.github.com>
2023-06-08 00:53:42 -0300

    add send_prompt

--- lmgdataset/gptscraper.py
+++ lmgdataset/gptscraper.py
@@ -3,6 +3,7 @@ from time import sleep
 
 import undetected_chromedriver as uc
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.remote.webelement import WebElement
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
@@ -29,7 +30,7 @@ class GptScraper:
         element: WebElement = self.wait.until(
             EC.visibility_of_element_located((by, identifier))
         )
-        element.send_keys(keys)
+        element.send_keys(keys + Keys.ENTER)
         sleep(2)
 
     def get_openai(self) -> None:
@@ -39,13 +40,25 @@ class GptScraper:
     def log_in_google(self, user: str, password: str) -> None:
         self.click_element(By.XPATH, '//button/span[text()="Continue with Google"]')
         self.send_keys_element(By.XPATH, '//input[@id="identifierId"]', user)
-        self.click_element(By.XPATH, '//input[@type="password"]', password)
+        self.send_keys_element(By.XPATH, '//input[@type="password"]', password)
 
     def click_new_chat(self) -> None:
         self.click_element(By.XPATH, '//a[text()="New chat"]')
 
     def select_gpt_4(self) -> None:
         self.click_element(By.XPATH, '//span[text()="GPT-4"]')
+
+    def send_prompt(self, promt: str) -> str:
+        scraper.send_keys_element(By.XPATH, "//textarea[@id='prompt-textarea']", promt)
+        sleep(5)
+        response: WebElement = scraper.driver.find_element(
+            By.XPATH,
+            (
+                "//*[local-name() = 'svg']/*[local-name() = 'title' and text() = 'ChatGPT']"
+                "/ancestor::div[4]//p"
+            ),
+        )
+        return response.text
 
 
 if __name__ == "__main__":
@@ -55,3 +68,7 @@ if __name__ == "__main__":
     scraper.get_openai()
     scraper.log_in_google()
     logging.info("Scraper started successfully.")
+    scraper.click_new_chat()
+    scraper.select_gpt_4()
+    response = scraper.send_prompt("hey this is a test")
+    logging.info(f"The response was {response}.")
